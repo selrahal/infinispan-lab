115db30a053c5333b9090a3d20799f34dac6e374
Salem Elrahal <salemelrahal@gmail.com>
2015-02-14 16:18:18 -0500

    Refactored logic for creating caches and cachemanagers

--- basic/src/main/java/com/rhc/jdg/App.java
+++ basic/src/main/java/com/rhc/jdg/App.java
@@ -2,19 +2,17 @@ package com.rhc.jdg;
 
 import java.util.Map;
 
-import org.infinispan.manager.DefaultCacheManager;
-import org.infinispan.manager.EmbeddedCacheManager;
-
+import com.rhc.jdg.util.CacheProvider;
 import com.rhc.jdg.util.MapUtil;
 
 public class App {
+	private static CacheProvider cacheProvider = new CacheProvider();
+	
 	public static void main(String[] args) {
-		EmbeddedCacheManager cacheManager = new DefaultCacheManager();
-		Map<String, String> cache = cacheManager.getCache();
+		Map<String, String> cache = cacheProvider.getCache();
 		cache.put("key1", "value1");
 		cache.put("key2", "value2");
 		System.out.println("Cache " + cache + " size:" + cache.size());
 		System.out.println(MapUtil.contents(cache));
-		
 	}
 }
