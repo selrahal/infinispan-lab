b509458975dea9c5b4396d4bde34f831460952be
Salem Elrahal <salemelrahal@gmail.com>
2015-02-15 16:23:11 -0500

    Updated basic project to show all three methods of getting caches

--- basic/src/main/java/com/rhc/jdg/util/CacheProvider.java
+++ basic/src/main/java/com/rhc/jdg/util/CacheProvider.java
@@ -6,6 +6,7 @@ import org.infinispan.manager.DefaultCacheManager;
 
 public class CacheProvider {
 	private CacheContainer cacheManager;
+	private CacheContainer remoteCacheManager;
 	
 	public CacheContainer getCacheManager() {
 		if (cacheManager == null) {
@@ -14,7 +15,18 @@ public class CacheProvider {
 		return cacheManager;
 	}
 	
-	public Cache getCache() {
+	public CacheContainer getRemoteCacheManager() {
+		if (cacheManager == null) {
+			cacheManager = new DefaultCacheManager();
+		}
+		return cacheManager;
+	}
+	
+	public Cache<String, String> getLibraryCache() {
 		return getCacheManager().getCache();
+	}
+	
+	public Cache<String, String> getRemoteCache() {
+		return getRemoteCacheManager().getCache();
 	}
 }
