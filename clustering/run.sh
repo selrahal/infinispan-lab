bdb87bcb3650ec72d038f5d0d8383792a149b9b8
Salem Elrahal <salemelrahal@gmail.com>
2015-02-17 11:55:16 -0500

    Updated clustering project so library mode clusters correctly

--- clustering/run.sh
+++ clustering/run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-mvn exec:java -Dexec.mainClass=com.rhc.jdg.App
+mvn exec:java -Dexec.mainClass=com.rhc.jdg.App -Djava.net.preferIPv4Stack=true -Djgroups.bind_addr=127.0.0.1
--- clustering/src/main/java/com/rhc/jdg/App.java
+++ clustering/src/main/java/com/rhc/jdg/App.java
@@ -11,14 +11,14 @@ import com.rhc.jdg.provider.XmlCacheProvider;
 import com.rhc.jdg.util.MapUtil;
 
 public class App {
-//	private static JavaCacheProvider javaCacheProvider = new JavaCacheProvider();
+	private static JavaCacheProvider javaCacheProvider = new JavaCacheProvider();
 //	private static XmlCacheProvider xmlCacheProvider = new XmlCacheProvider();
-	private static RemoteCacheProvider remoteCacheProvider = new RemoteCacheProvider();
+//	private static RemoteCacheProvider remoteCacheProvider = new RemoteCacheProvider();
 	
 	public static void main(String[] args) {
-//		Map<String, String> javaCache = javaCacheProvider.getCache();
+		Map<String, String> javaCache = javaCacheProvider.getCache();
 //		Map<String, String> xmlCache = xmlCacheProvider.getCache();
-		RemoteCache<String, String> remoteCache = remoteCacheProvider.getCache();
+//		RemoteCache<String, String> remoteCache = remoteCacheProvider.getCache();
 		
 		boolean running = true;
 		Scanner input = new Scanner(System.in);
@@ -32,13 +32,13 @@ public class App {
 				String key = input.next();
 				System.out.print("Value:");
 				String value = input.next();
-				remoteCache.put(key, value);
+				javaCache.put(key, value);
 			} else if (choice.equalsIgnoreCase("r")) {
 				System.out.print("Key:");
 				String key = input.next();
-				remoteCache.remove(key);
+				javaCache.remove(key);
 			} else if (choice.equalsIgnoreCase("l")) {
-				System.out.println(MapUtil.contents(remoteCache));
+				System.out.println(MapUtil.contents(javaCache));
 			} else {
 				System.out.println("Unknown input!");
 			}
