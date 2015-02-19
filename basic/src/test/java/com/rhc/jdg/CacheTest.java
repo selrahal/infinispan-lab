7e41d265763a357414a762d95c1e08b7dc35d0dd
Salem Elrahal <salemelrahal@gmail.com>
2015-02-19 14:02:04 -0500

    Added useful messages to asserts in tests

--- basic/src/test/java/com/rhc/jdg/CacheTest.java
+++ basic/src/test/java/com/rhc/jdg/CacheTest.java
@@ -9,7 +9,6 @@ import org.junit.Test;
 import com.rhc.jdg.provider.JavaCacheProvider;
 import com.rhc.jdg.provider.RemoteCacheProvider;
 import com.rhc.jdg.provider.XmlCacheProvider;
-import com.rhc.jdg.util.MapUtil;
 
 public class CacheTest {
 	
@@ -19,9 +18,9 @@ public class CacheTest {
 		Map<String, String> javaCache = javaCacheProvider.getCache();
 		javaCache.put("key1", "value1");
 		javaCache.put("key2", "value2");
-		Assert.assertEquals(2, javaCache.size());
-		Assert.assertEquals("value1", javaCache.get("key1"));
-		Assert.assertEquals("value2", javaCache.get("key2"));
+		Assert.assertEquals("Cache size is incorrect", 2, javaCache.size());
+		Assert.assertEquals("Value for key1 is incorrect", "value1", javaCache.get("key1"));
+		Assert.assertEquals("Value for key2 is incorrect", "value2", javaCache.get("key2"));
 	}
 	
 	@Test
@@ -30,9 +29,9 @@ public class CacheTest {
 		Map<String, String> xmlCache = xmlCacheProvider.getCache();
 		xmlCache.put("key1", "value1");
 		xmlCache.put("key2", "value2");
-		Assert.assertEquals(2, xmlCache.size());
-		Assert.assertEquals("value1", xmlCache.get("key1"));
-		Assert.assertEquals("value2", xmlCache.get("key2"));
+		Assert.assertEquals("Cache size is incorrect", 2, xmlCache.size());
+		Assert.assertEquals("Value for key1 is incorrect", "value1", xmlCache.get("key1"));
+		Assert.assertEquals("Value for key2 is incorrect", "value2", xmlCache.get("key2"));
 	}
 	
 	@Test
@@ -41,8 +40,8 @@ public class CacheTest {
 		RemoteCache<String, String> remoteCache = remoteCacheProvider.getCache();
 		remoteCache.put("key1", "value1");
 		remoteCache.put("key2", "value2");
-		Assert.assertEquals(2, remoteCache.size());
-		Assert.assertEquals("value1", remoteCache.get("key1"));
-		Assert.assertEquals("value2", remoteCache.get("key2"));
+		Assert.assertEquals("Cache size is incorrect", 2, remoteCache.size());
+		Assert.assertEquals("Value for key1 is incorrect", "value1", remoteCache.get("key1"));
+		Assert.assertEquals("Value for key2 is incorrect", "value2", remoteCache.get("key2"));
 	}
 }
