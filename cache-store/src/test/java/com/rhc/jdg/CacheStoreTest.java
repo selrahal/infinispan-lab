684c4ad0aaff2d9b7c55cfb2c867817084890da0
Salem Elrahal <salemelrahal@gmail.com>
2015-02-19 16:19:10 -0500

    Added tests for cache store

---
+++ cache-store/src/test/java/com/rhc/jdg/CacheStoreTest.java
@@ -0,0 +1,37 @@
+package com.rhc.jdg;
+
+import org.infinispan.Cache;
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.rhc.jdg.provider.JavaCacheProvider;
+import com.rhc.jdg.provider.XmlCacheProvider;
+
+public class CacheStoreTest {
+	
+	@Test
+	public void testJavaCache() {
+		JavaCacheProvider javaCacheProvider = new JavaCacheProvider();
+		Cache<String, String> cache = javaCacheProvider.getCache();
+		cache.put("key1", "value1");
+		cache.put("key2", "value2");
+		cache.evict("key1");
+		cache.evict("key2");
+		Assert.assertEquals("Cache does not have entries", 2, cache.size());
+		Assert.assertEquals("value1", cache.get("key1"));
+		Assert.assertEquals("value2", cache.get("key2"));
+	}
+	
+	@Test
+	public void testXmlCache() {
+		XmlCacheProvider xmlCacheProvider = new XmlCacheProvider();
+		Cache<String, String> cache = xmlCacheProvider.getCache();
+		cache.put("key1", "value1");
+		cache.put("key2", "value2");
+		cache.evict("key1");
+		cache.evict("key2");
+		Assert.assertEquals("Cache does not have entries", 2, cache.size());
+		Assert.assertEquals("value1", cache.get("key1"));
+		Assert.assertEquals("value2", cache.get("key2"));
+	}
+}
